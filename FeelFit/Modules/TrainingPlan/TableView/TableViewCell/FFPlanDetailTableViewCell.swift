e39427a40df5a24b3236e897b72aec4850b8d04a
Константин Малков <kmalkov_1995@mail.ru>
2023-12-18 22:33:13 +0300

    -added custom cell for table  view -added segue by pressing notification

---
+++ FeelFit/Modules/TrainingPlan/TableView/TableViewCell/FFPlanDetailTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FFPlanDetailTableViewCell.swift
+//  FeelFit
+//
+//  Created by Константин Малков on 18.12.2023.
+//
+
+import UIKit
+
+class FFPlanDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
